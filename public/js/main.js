5271f9890e6eeb77b69c3b8989f95e9e9997aac1
warlinsgit <warlins25@gmail.com>
2019-04-18 22:42:26 +0100

    git hero

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ form.addEventListener('submit', e => {
   const data = {cafe: choice};
 
 //  fetch('http://localhost:3000/poll', {
-    fetch('https://gentle-springs-57466.herokuapp.com:3000/poll', {
+    fetch('http://localhost:3000/poll', {
     method: 'post',
     body: JSON.stringify(data),
     headers: new Headers ({
@@ -21,7 +21,7 @@ form.addEventListener('submit', e => {
   e.preventDefault();
 });
 
-fetch("https://gentle-springs-57466.herokuapp.com:3000/poll")
+fetch("http://localhost:3000/poll")
 .then(res => res.json())
 .then(data => {
 
