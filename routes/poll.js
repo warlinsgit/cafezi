d136bad42fc5dc3e57fae90babed114df09824b6
warlinsgit <warlins25@gmail.com>
2019-04-18 18:48:47 +0100

    mst

--- routes/poll.js
+++ routes/poll.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require("mongoose");
-const Vote = require('../models/vote');
+const Vote = require('./models/vote');
 const Pusher = require('pusher');
 
 var pusher = new Pusher({
