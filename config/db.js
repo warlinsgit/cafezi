bcda9e8410adc56a7b796d988768aa47b33babdd
warlinsgit <33092160+warlinsgit@users.noreply.github.com>
2019-08-26 22:12:04 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //map global promisses
 //mongoose.Promise = global.Promise;
 // mongoose connect
-mongoose.connect( "mongodb+srv://warlins123:warlins123@warlins123-wbteo.mongodb.net/pusherpoll2?retryWrites=true")
+mongoose.connect( "")
 
 
 .then(()=> console.log('MongoDB Connected'))
